7745c5296bcf0577ff28f3e350abe755568e5010
xingguozm <18969176324@163.com>
2022-10-10 08:55:05 +0000

    svgCircle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CircleDemo from "./pages/CircleDemo";
+import SvgCircleDemo from './pages/SvgCircleDemo';
 
 export default function App() {
   return (
     <div>
       <CircleDemo />
+      <SvgCircleDemo/>
     </div>
   );
 }
